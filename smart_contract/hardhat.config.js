fa66dab6af608e5546b5aab822d02d6b0d55e3fc
Rushikesh0020 <rushikeshpatil0020@gmail.com>
2022-01-25 13:44:21 +0530

    Modfied config file, created solidity smart contract and written a script to deploy contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+// https://eth-ropsten.alchemyapi.io/v2/3kB5AjTFznYokjlKCZbwOpwbGq4NSfjX
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require('@nomiclabs/hardhat-waffle')
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/3kB5AjTFznYokjlKCZbwOpwbGq4NSfjX',
+      accounts: ['7b2f5091b8720b628a7c13ef3b5f8d67acdf82cad2c0d861ebc33150cf0d1c1b']
+    }
+  }
+}
